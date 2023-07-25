757b7b23564aa351b46617d75b7426703e8ac406
Bharat Mediratta <bharat@menalto.com>
2023-07-25 09:57:37 +0200

    Initial Commit

---
+++ Chungler/ChunglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChunglerApp.swift
+//  Chungler
+//
+//  Created by Bharat Mediratta on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ChunglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
